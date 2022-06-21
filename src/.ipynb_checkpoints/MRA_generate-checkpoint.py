fbb1f145a1a9db61d9e2446e4e18b1dc9fe6bacb
Sr-11 <eruisun@mit.edu>
2022-06-21 13:50:15 -0400

    change MRA_generate, merge __init__ into generate_default()

--- src/.ipynb_checkpoints/MRA_generate-checkpoint.py
+++ src/.ipynb_checkpoints/MRA_generate-checkpoint.py
@@ -6,50 +6,61 @@ import timeit
 def g0(n,x):
     return np.random.rand(1)
 class MRA_generate():
+    '''
+    Parameters
+    ----------
+    d : int
+        The dimensions of each instance, say d=dim Xi[j] (e.g. d=28*28 for mnist).
+    nt : int
+        The number of instances in each X_i, say n_tau in the paper (Xi[1]...Xi[nt]).
+    N : int
+        Cardinality of the data set X, say n_X in the paper.
+    sigma : float
+        The standard deviation of the noise of normal distribution.
+    ne : int
+        For convinence, remaining consistent with other generate_ functions.
+    g : function
+        g(n,x) n=0,1..., x∈(0,1)
+    replace : bool
+        if replace==True, Xi[j] (i fixed, j different) may correspond to same block in D.
+    outer_replace : bool
+        if outer_replace==False, Xi[j] are all different (for all i,j).
+
+    Yields
+    ----------
+    X : numpy.ndarray
+        Return the generated data set X, a N*nt*d numpy tensor.
+        X.shape=(N,nt,d)
+    SNR : numpy.ndarray
+        SNR.shape=(N,)
+        Signal-to-noise ratio
+    thetas : numpy.ndarray
+        theta.shape=(N,d)
+    shifts : numpy.ndarray
+        shifts.shape=(N,nt)
+    states : numpy.ndarray
+        states.shape=(N,)
+        states is used to mark how many different states are in X
+        doesn't have uniform format
+    '''
     def __init__(self,d=100,nt=20,N=10000,sigma=0,ne=20,g=g0,replace=False,outer_replace=True):
-        '''
-        Parameters
-        ----------
-        d : int
-            The dimensions of each instance, say d=dim Xi[j] (e.g. d=28*28 for mnist).
-        nt : int
-            The number of instances in each X_i, say n_tau in the paper (Xi[1]...Xi[nt]).
-        N : int
-            Cardinality of the data set X, say n_X in the paper.
-        sigma : float
-            The standard deviation of the noise of normal distribution.
-        ne : int
-            For convinence, remaining consistent with other generate_ functions.
-        g : function
-            g(n,x) n=0,1..., x∈(0,1)
-        replace : bool
-            if replace==True, Xi[j] (i fixed, j different) may correspond to same block in D.
-        outer_replace : bool
-            if outer_replace==False, Xi[j] are all different (for all i,j).
-            
-        Yields
-        ----------
-        X : numpy.ndarray
-            Return the generated data set X, a N*nt*d numpy tensor.
-            X.shape=(N,nt,d)
-        SNR : numpy.ndarray
-            SNR.shape=(N,)
-            Signal-to-noise ratio
-        thetas : numpy.ndarray
-            theta.shape=(N,d)
-        shifts : numpy.ndarray
-            shifts.shape=(N,nt)
-        states : numpy.ndarray
-            states.shape=(N,)
-            states is used to mark how many different states are in X
-            doesn't have uniform format
-        '''
         self.d=d
         self.nt=nt
         self.N=N
         self.sigma=sigma
         self.ne=ne
+        self.g=g
         self.replace=replace
+        self.outer_replace=outer_replace
+    def generate_default(self):
+        d = self.d
+        nt = self.nt
+        N = self.N
+        sigma = self.sigma
+        ne = self.ne
+        g = self.g
+        replace = self.replace
+        outer_replace = self.outer_replace
         X=np.empty((N,nt,d), dtype = float, order = 'C')
         self.X=X
         states=np.empty(N, dtype = int)
@@ -83,26 +94,9 @@ class MRA_generate():
                 shifts[i,j]=l
                 for k in range(d):
                     X[i,j,k]=thetas[i,(k+l)%d]+sigma*np.random.normal()
-           
+        return X
         
     """
-    def generate_default(self):
-        d=self.d;nt=self.nt;N=self.N;sigma=self.sigma;ne=self.ne;states=self.states
-        X=self.X;SNR=self.SNR;thetas=self.thetas;shifts=self.shifts;g=self.g
-        for i in range(N):
-            e=np.random.randint(ne)
-            states[i]=e
-            thetas[i,:]=[g(e,k/d) for k in range(d)]
-            if sigma!=0:
-                SNR[i]=(np.linalg.norm(thetas[i,:],2)/sigma)**2
-            else:
-                SNR[i]=np.inf
-            for j in range(nt):
-                l=np.random.randint(d)
-                shifts[i,j]=l
-                for k in range(d):
-                    X[i,j,k]=thetas[i,(k+l)%d]+sigma*np.random.normal()
-        return X
     def generate_random(self):
         '''
         Generate a Multireference Alignment (MRA) data set X. 
