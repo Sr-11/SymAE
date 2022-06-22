5d9e7af26f234e28098ca5fb4cb84a92879c6b7a
Sr-11 <eruisun@mit.edu>
2022-06-21 20:44:13 -0400

    add matshow()

--- rank1/.ipynb_checkpoints/redatuming-checkpoint.py
+++ rank1/.ipynb_checkpoints/redatuming-checkpoint.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tqdm import trange
-def redatuming(model,data):
+def redatuming(model,data,i,i_prime):
     X = data.X
     X_states = data.X_states
     X_nuisances = data.X_nuisances
@@ -9,11 +9,9 @@ def redatuming(model,data):
     Z_hat=np.empty((nx,nx,nt))
     Cs=model.sym_encoder.predict(X, verbose=0)
     Ns=model.nui_encoder.predict(X, verbose=0)
-    for i in trange(5):
-        for i_prime in range(5):
-            coherent_i_prime=Cs[i_prime:i_prime+1,:]
-            nuisance_i_j=Ns[i_prime:i_prime+1,:]
-            merger = model.latentcat(coherent_i_prime,nuisance_i_j)
-            Z_hat[i,i_prime,:]=model.decoder.predict(merger, verbose=0)[0,:,0]
-            Z[i,i_prime,:]=X_states[i_prime]*X_nuisances[i,:] 
-    return Z,Z_hat
+    coherent_i_prime=Cs[i_prime:i_prime+1,:]
+    nuisance_i_j=Ns[i_prime:i_prime+1,:]
+    merger = model.latentcat(coherent_i_prime,nuisance_i_j)
+    #Z_hat[i,i_prime,:]=model.decoder.predict(merger, verbose=0)[0,:,0]
+    #Z[i,i_prime,:]=X_states[i_prime]*X_nuisances[i,:] 
+    return model.decoder.predict(merger, verbose=0)[0,:,0], X_states[i_prime]*X_nuisances[i,:] 
